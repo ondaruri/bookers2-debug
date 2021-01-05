0fd0204011c15ec6d57680c972ed0d979e53d819
ondaruri <f3raser8cg@yahoo.co.jp>
2021-01-05 00:42:13 +0000

    teisyutu

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
+    @books = @user.books
+  end
+
+  def edit
+    @user = current_user
+    if params[:id].to_i == current_user.id
+      render :edit
+    else
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @books = @user.books
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
