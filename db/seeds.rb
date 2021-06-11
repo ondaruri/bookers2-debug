b6eaff6cbb4d477f063829fb617f329c79566607
ondaruri <f3raser8cg@yahoo.co.jp>
2021-06-11 07:39:00 +0000

    seed 画像

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+   email: 'uri@uri',
+   name: 'uri',
+   password: 'uriuri',
+  profile_image: File.open("./app/assets/images/人面犬.png")
+)
